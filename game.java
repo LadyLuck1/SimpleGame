9d0e3b088684366f39fe0b672f8c92ffe0056446
LadyLuck1 <berch_beer@yahoo.com>
2014-06-30 11:03:59 -0400

    Create game.java

---
+++ game.java
@@ -0,0 +1,19 @@
+import java.util.Scanner
+import java.oi.*;
+
+public void main(){
+
+  Ball ball1 = new Ball();
+  Ball ball2 = new Ball();
+  Scanner sc = new Scanner(System.in);
+  int powerup1 = 1;
+  int choice;
+
+system.out.println("Guess a number between 1 - 10: ");
+choice = sc.getInt();
+
+  if(guess == choice)
+    ball1+= powerup1;
+    
+
+}
